3adf070934ac67a782fe4ed1fa6ad039955ca13c
Trần Hải Đăng <106256609+DangK3@users.noreply.github.com>
2024-07-09 23:23:38 +0700

    BaseUI

---
+++ K_Bridge/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K_Bridge.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("/UserProfile/{UserID}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
