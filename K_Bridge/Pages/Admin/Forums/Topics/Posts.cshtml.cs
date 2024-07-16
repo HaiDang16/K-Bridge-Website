390d16d34eabdb3d105089fbe87d435bfcf2ba0b
Trần Hải Đăng <106256609+DangK3@users.noreply.github.com>
2024-07-16 16:19:40 +0700

    Admin/Forums BaseUI

---
+++ K_Bridge/Pages/Admin/Forums/Topics/Posts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace K_Bridge.Pages.Admin.Forums.Topic
+{
+    public class PostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
